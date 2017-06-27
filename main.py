4150631366fb0dd77cd082da6290c15a024b0a55
JakesCode <jakestr1999@gmail.com>
2017-06-27 19:55:50 +0100

    events system fully implemented. added support for 'speech' / dialogue! removed last two locations - beginning to develop a real storyline now. added support for both item and location events, simply by calling id numbers in a list.

--- main.py
+++ main.py
@@ -23,8 +23,35 @@ class Game:
                         for itemIndex in itemsToAdd:
                             # Create Item object(s) #
                             newItem = Item(id_num = itemData["items"][itemIndex]["id"], itemName = itemData["items"][itemIndex]["name"], itemDescription = itemData["items"][itemIndex]["description"])
-                            newLocation.items[newItem.name.lower()] = newItem
+
+                            # Some items have events, too. #
+                            try:
+                                events = data["locations"][index]["events"]
+                                for event in events:
+                                    # 'event' will be an ID that corresponds to an ID in the events.json file #
+                                    with open('events.json') as eventsFile:
+                                        eventsData = json.load(eventsFile)["events"]
+                                        # Add the linked event to the newLocation object #
+                                        newEvent = Event(id_num=eventsData[event]["id"], data=eventsData[event]["data"])
+                                        newItem.events[event] = newEvent
+                            except:
+                                pass
+                        newLocation.items[newItem.name.lower()] = newItem
+
                 except KeyError:
+                    pass
+
+                # Some locations have events - try/except block scans locations and adds them to Location objects #
+                try:
+                    events = data["locations"][index]["events"]
+                    for event in events:
+                        # 'event' will be an ID that corresponds to an ID in the events.json file #
+                        with open('events.json') as eventsFile:
+                            eventsData = json.load(eventsFile)["events"]
+                            # Add the linked event to the newLocation object #
+                            newEvent = Event(id_num=eventsData[event]["id"], data=eventsData[event]["data"])
+                            newLocation.events[event] = newEvent
+                except:
                     pass
 
                 # Add to Game.locations dictionary #
@@ -44,24 +71,34 @@ class Game:
             self.player.location = self.locations[self.player.position]
 
             # 2 - Display HUD #
-            print("Current Location:", self.player.location.name)
-            print(self.player.health, "HP")
-            print("Map:", end=' ')
-            gameMap = list("-"*len(self.locations))
-            for x in range(0, len(gameMap)):
-                if x == self.player.position:
-                    print("*", end="")
-                else:
-                    print(gameMap[x], end="")
-            print("\n")
+            self.HUD()
 
             # 3 - Check for items #
-            if self.player.location.checkForItems():
-                print("There are items here!")
+            #if self.player.location.checkForItems():
+            #    print("There are items here!")
 
-            # 4 - User prompt #
+            # 4 - Check for events at this location #
+            if self.player.location.checkForEvents():
+                self.player.location.runEvents()
+                # Add the HUD again #
+                s("cls")
+                self.HUD()
+
+            # 5 - User prompt #
             self.command(input("\n---> What now? "))
             input()
+
+    def HUD(self):
+        print("Current Location:", self.player.location.name)
+        print(self.player.health, "HP")
+        print("Map:", end=' ')
+        gameMap = list("-"*len(self.locations))
+        for x in range(0, len(gameMap)):
+            if x == self.player.position:
+                print("*", end="")
+            else:
+                print(gameMap[x], end="")
+        print("\n")
 
     def command(self, user):
         # Prevent any erroneous capitalisations #
@@ -94,6 +131,9 @@ class Game:
                 individualElements = item.split(' ')
                 try:
                     if user[1] in individualElements:
+                        # Item may be linked to an event - check for this #
+                        if len(self.player.location.items[item].events) > 0:
+                            self.player.location.items[item].runEvents()
                         # Add to inventory, remove from location #
                         self.player.inventory[item] = self.player.location.items[item]
                         del self.player.location.items[item]
@@ -108,7 +148,6 @@ class Game:
                     else:
                         print("Can't find that item.")
 
-
         # INV / INVENTORY #
         if user[0] in ["inv", "inventory"]:
             self.player.displayInventory()
@@ -119,12 +158,27 @@ class Location:
         self.id = id_num
         self.name = name
         self.items = {}
+        self.events = {}
+
+    def checkForEvents(self):
+        if len(self.events) > 0:
+            return True
+        else:
+            return False
 
     def checkForItems(self):
         if len(self.items) > 0:
             return True
         else:
             return False
+
+    def runEvents(self):
+        for event in self.events:
+            eventKeys = list(self.events[event].data.keys())
+            for singleEvent in eventKeys:
+                if singleEvent == "speech":
+                    self.events[event].execute(singleEvent)
+        self.events.clear()
 
     def displayItems(self):
         for item in list(self.items.keys()):
@@ -163,12 +217,54 @@ class Item:
         self.id = id_num
         self.name = itemName
         self.description = itemDescription
+        self.events = {}
+
+    def runEvents(self):
+        for event in self.events:
+            eventKeys = list(self.events[event].data.keys())
+            for singleEvent in eventKeys:
+                if singleEvent == "speech":
+                    self.events[event].execute(singleEvent)
+        # Events have completed - remove from events dictionary to avoid it happening multiple times #
+        self.events.clear()
+
 
 class Event:
-    def __init__(self, id_num, eventType):
-        self.id = id_num,
-        self.type = eventType
+    def __init__(self, id_num, data):
+        self.id = id_num
+        self.data = data
 
+    def execute(self, eventType):
+        if eventType == "speech":
+            # This is a dialogue event #
+            with open('speech.json') as speechFile:
+                speechData = json.load(speechFile)["speech"][self.id]
+                DialogueEvent = Dialogue(id_num=speechData["id"], characters=speechData["characters"], data=speechData["data"])
+
+class Dialogue:
+    def __init__(self, id_num, characters, data):
+        self.id = id_num
+        self.characters = characters
+        self.data = data
+        self.read()
+
+    def read(self):
+        # The ID key in the data dictionary corresponds to list indices in the self.characters list. #
+        lineKeys = list(self.data.keys())
+        for turn in range(0, len(self.data)):
+            for line in self.data[lineKeys[turn]]:
+                s("cls")
+                # The person saying the line #
+                lengthOfLineCalculation = len(line) - len(self.characters[int([lineKeys[turn]][0])])
+                lengthOfLine = int(round(lengthOfLineCalculation / 2))
+                print("-"*lengthOfLine + self.characters[int([lineKeys[turn]][0])] + "-"*lengthOfLine)
+                # The line #
+                print(line, "\n")
+                offset = round(len(self.characters[int([lineKeys[turn]][0])]))/4+1
+                input(' '*int(round(lengthOfLine+offset)) + "(cont.)")
+
+
+s("title Some Python Adventure Game")
 MainGame = Game()
 MainGame.gameLoop()
 input()
